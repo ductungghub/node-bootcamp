23fc5135995b42899ce598d37f43c7088fb0f152
tung <ndtung723@gmail.com>
2022-08-01 10:30:03 +0700

    Handling GET Requests

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+const fs = require('fs');
 const express = require('express');
-
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('hello from the server side');
-// })
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .json({ message: 'hello from the server side', app: 'Natours' });
-})
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    });
+});
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`app running on the ${port}`);
-})
+});
