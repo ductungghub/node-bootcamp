128deab1f3a0c03d113956e7a405999604478e71
tung <ndtung723@gmail.com>
2022-08-11 17:47:41 +0700

    Environment Variables + Setting up ESLint + Prettier in VS Code

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+     app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
