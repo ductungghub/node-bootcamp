69d0f68785c8d6e29719732b30a1cd7a9fb6f39b
tung <ndtung723@gmail.com>
2022-10-23 04:47:21 +0700

    reading documents

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const tourRouter = require('./routes/tourRoutes');
+const tourRouter = require("./routes/tourRoutes");
 
-const userRouter = require('./routes/userRoutes');
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // 1) MIDDLEWARES
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
+  console.log("Hello from the middleware🙌");
   next();
 });
 
@@ -27,8 +27,8 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 // 4) START SERVER
 
