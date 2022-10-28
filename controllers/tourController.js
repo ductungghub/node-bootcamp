f05302deff013bdd0bac958744e04491d6021f4b
tung <ndtung723@gmail.com>
2022-10-28 19:52:21 +0700

    Making the API Better Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,11 +3,17 @@ const Tour = require("../models/tourModel");
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
+    //  2) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = await Tour.find(queryObj);
+    //  2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = await Tour.find(JSON.parse(queryStr));
 
     //EXCUTE QUERY
     const tours = await query;
